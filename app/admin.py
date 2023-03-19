f8cfe43a62554a30de3326ade057f967a23fb76c
Zhixian Qi <2815122Q@student.gla.ac.uk>
2023-03-19 21:47:42 +0000

    refine

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app.models import UserProfile
-# Register your models here.
-
-admin.site.register(UserProfile)
+# from app.models import UserProfile
+# # Register your models here.
+#
+# admin.site.register(UserProfile)
--- app/models.py
+++ app/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-
-    def __str__(self):
-        return self.user.username
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     # The additional attributes we wish to include.
+#     website = models.URLField(blank=True)
+#     picture = models.ImageField(upload_to='profile_images', blank=True)
+#
+#     def __str__(self):
+#         return self.user.username
