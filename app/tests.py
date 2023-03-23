4a1c5831d8ecda6ddcdb6eb271cd799766fa6545
LujingCai <122382748+LujingCai@users.noreply.github.com>
2023-03-23 16:20:03 +0000

    add unit test for register and login

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class AuthenticationTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('registration_register')
+        self.login_url = reverse('auth_login')
+
+    def test_register(self):
+        response = self.client.post(self.register_url, {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123'
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_login(self):
+        user = User.objects.create_user(username='testuser', password='testpassword123')
+
+        response = self.client.post(self.login_url, {
+            'username': 'testuser',
+            'password': 'testpassword123'
+        }, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        user = User.objects.get(username='testuser')
+        self.assertTrue(user.is_authenticated)
+
+
